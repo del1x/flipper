825967e289f38991ca3135a717bd351bd9961ed2
del1x <37791473+del1x@users.noreply.github.com>
2021-11-22 19:43:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const colors = ["red", "blue", "#002451", "rgb(147 22 188)", "#267DC2"];
+const btn = document.getElementById("btn");
+const colorCode = document.querySelector(".color-code");
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorCode.textContent = colors[randomNumber];
+    colorCode.style.color = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
